dab4618e4159f30706185a3aefde46ec0d4afb4c
Daniele Anselmo <daniele.anselmo91@gmail.com>
2023-03-06 14:52:47 +0100

    notifications

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ChirpRequest;
 use App\Models\Chirp;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -15,7 +16,9 @@ class ChirpController extends Controller
      */
     public function index(): Response
     {
-       return Inertia::render('Chirps/Index', []);
+       return Inertia::render('Chirps/Index', [
+        'chirps' => Chirp::with('user:id,name')->latest()->get(),
+       ]);
     }
 
     /**
@@ -59,16 +62,29 @@ class ChirpController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Chirp $chirp)
+    public function update(Request $request, Chirp $chirp): RedirectResponse
     {
-        //
+        $this->authorize('update', $chirp);
+
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+
+
+        $chirp->update($validated);
+
+        return redirect(route('chirps.index'));
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Chirp $chirp)
+    public function destroy(Chirp $chirp): RedirectResponse
     {
-        //
+        $this->authorize('delete', $chirp);
+
+        $chirp->delete();
+
+        return redirect(route('chirps.index'));
     }
 }
